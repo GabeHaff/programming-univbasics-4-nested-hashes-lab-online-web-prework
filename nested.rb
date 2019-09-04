d045f2ecba44b46524a9d04b32302b5b213e7a4f
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-04 20:02:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -68,7 +68,6 @@ def adding_matz
 
 
 	programmer_hash = 
- 	_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",:languages => ["LISP", "C"]}
 
  		{
         :grace_hopper => {
@@ -85,7 +84,7 @@ def adding_matz
         },
  		}
      }
-  
+  programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",:languages => ["LISP", "C"]}
 return programmer_hash
     
 end
