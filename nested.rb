c78675dadc97f8ca10786a398a797479aad3ac7a
Gabriel Hafferty <gabe.hafferty.business@gmail.com>
2019-09-04 20:21:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,6 +1,6 @@
 
 def hopper
-  #operates on programmer_hash and returns value of grace_hopper key
+  
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -21,7 +21,7 @@ programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+	
 	
 	programmer_hash = 
  		{
@@ -58,15 +58,11 @@ def dennis_ritchies_language
         }
      }
      programmer_hash[:dennis_ritchie][:languages][0]
-     #returns value of dennies ritchies language as a string 
+      
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
+
 
 
 	programmer_hash = 
@@ -105,7 +101,7 @@ def changing_alan
           :languages => ["C"]
         }
      }
-     #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
+      
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for]="GUI"
      return programmer_hash
@@ -129,6 +125,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages]="C","Assembly" #adds "assembly" to "known for"
+programmer_hash[:dennis_ritchie][:languages]="C","Assembly"
 return programmer_hash
 end
